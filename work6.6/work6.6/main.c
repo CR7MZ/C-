89efc4b6f58c53126db504a432ca26cdc753252b
李明哲 <963835264@qq.com>
2019-08-06 16:28:34 +0800

    用递归方式实现打印一个整数的每一位。

---
+++ work6.6/work6.6/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void Print(int n)//ตÝน้
+{
+	if (n > 9)
+	{
+		Print(n / 10);
+	}
+	printf("%d\n", n % 10);
+}
+int main()
+{
+	Print(1234);
+	system("pause");
+	return 0;
+}
